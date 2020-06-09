2e397712dfa37805f75066b629e840c6c4864f40
leandro <leandrocabralstudent@gmail.com>
2020-06-08 21:35:17 -0300

    Added component navbottom

--- presentation/src/main/java/com/leandroid/formula1/MainActivity.kt
+++ presentation/src/main/java/com/leandroid/formula1/MainActivity.kt
@@ -5,15 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import com.leandroid.data.network.race.RemoteRaceService
 import com.leandroid.data.network.service.RaceService
+import com.leandroid.formula1.home.HomeFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_main, HomeFragment())
+            .commit()
 
     }
 
--- presentation/src/main/java/com/leandroid/formula1/widget/NavigationBottom.kt
+++ presentation/src/main/java/com/leandroid/formula1/widget/NavigationBottom.kt
@@ -3,11 +3,14 @@ package com.leandroid.formula1.widget
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.leandroid.formula1.R
 
 class NavigationBottom
 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-
+    init {
+        inflate(context, R.layout.widget_navigation_bottom, this)
+    }
 }
