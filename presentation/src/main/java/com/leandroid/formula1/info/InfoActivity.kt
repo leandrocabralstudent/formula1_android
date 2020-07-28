843a6280c275cfa860c2c4bb15fd10216b84c58b
Leandro Cabral <leandrocabralf@gmail.com>
2020-07-28 16:08:25 -0300

    Adjust layout info

--- presentation/src/main/java/com/leandroid/formula1/info/InfoActivity.kt
+++ presentation/src/main/java/com/leandroid/formula1/info/InfoActivity.kt
@@ -16,7 +16,7 @@ class InfoActivity : AppCompatActivity() {
 
     private  val viewModel : InfoViewModel by viewModel()
     private lateinit var binding: InfoActivityBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.info_activity)
